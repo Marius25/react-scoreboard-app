922f1b9006311c339e32c602f8b2021ee0c4369c
Marius25 <m.reinys@gmail.com>
2017-05-17 18:21:13 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+
+//CSS
+import './css/style.css'
+
+//Components
+import Scoreboard from './components/Scoreboard';
+
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
